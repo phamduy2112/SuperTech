a2fb728ed137cf7c21e94246c4801c08e4782607
quangsazami <150917803+quangsazami@users.noreply.github.com>
2024-10-19 22:09:12 +0700

    Update Search.tsx

--- client/src/page/Client/Search/Search.tsx
+++ client/src/page/Client/Search/Search.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-function Search() {
-  return (
-    <div>Search</div>
-  )
-}
-
-export default Search
+ <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+    </div>
