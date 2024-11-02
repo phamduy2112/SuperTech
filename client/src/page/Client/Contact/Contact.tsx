a4eb20b513249cea547da406c1ae0eb008bc12f6
Sazami <q12deadpool@gmail.com>
2024-11-02 20:38:07 +0700

    day liên hệ

--- client/src/page/Client/Contact/Contact.tsx
+++ client/src/page/Client/Contact/Contact.tsx
@@ -34,127 +34,122 @@ function Contact() {
     },
   ];
   return (
-      <div className="w-[80%] m-auto">
-      <div className="flex flex-col h-full justify-center items-center p-10 gap-7">
-        <span className="text-[32px] font-medium text-center p-5">
-          Liên hệ cho chúng tôi
-        </span>
-        <span className="text-[16px] font-light text-[#727171]">
-          đội ngũ chuyên nghiệp giàu kinh nghiệm phục vụ quý khách hàng{" "}
-        </span>
-        <div className="w-full h-[800px] relative flex items-start">
-          <div className=" w-full h-[289px]">
-            <div className="h-full relative flex items-center justify-center ">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.2285676306672!2d106.62416199839477!3d10.852793500000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a272ac90551%3A0xfdedca96b3ea5e15!2zQ8O0bmcgVmnDqm4gUGjhuqduIE3hu4FtIFF1YW5nIFRydW5nIC0gQ8O0bmcgVmnDqm4gUGjDosyAbiBNw6rMgG0gUXVhbmcgVHJ1bmcgLSBUw7QgS8O9!5e0!3m2!1sen!2s!4v1728981405828!5m2!1sen!2s"
-                width="100%"
-                height="100%"
-                loading="lazy"
-              ></iframe>
-              <div className="text-[40px] bg-[#8187be67] w-full h-full flex items-center justify-end absolute ">
-                <span className="text-white text-[50px] font-light">Kết nối với <span className="font-medium text-[65px] text-[#9103FF] flex "> SuperTech</span> </span>
-              </div>
+    <div className="w-full md:w-[80%] m-auto">
+    <div className="flex flex-col h-full justify-center items-center p-4 md:p-10 gap-5 md:gap-7">
+      <span className="text-[24px] md:text-[32px] font-medium text-center p-2 md:p-5">
+        Liên hệ cho chúng tôi
+      </span>
+      <span className="text-[14px] md:text-[16px] font-light text-[#727171]">
+        đội ngũ chuyên nghiệp giàu kinh nghiệm phục vụ quý khách hàng
+      </span>
+      <div className="w-full h-[full] md:h-[800px] relative flex flex-col md:flex-row items-start">
+      <div className=" w-full h-[289px]">
+      <div className="h-full relative flex items-center justify-center">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.2285676306672!2d106.62416199839477!3d10.852793500000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a272ac90551%3A0xfdedca96b3ea5e15!2zQ8O0bmcgVmnDqm4gUGjhuqduIE3hu4FtIFF1YW5nIFRydW5nIC0gQ8O0bmcgVmnDqm4gUGjDosyAbiBNw6rMgG0gUXVhbmcgVHJ1bmcgLSBUw7QgS8O9!5e0!3m2!1sen!2s!4v1728981405828!5m2!1sen!2s"
+              width="100%"
+              height="100%"
+              loading="lazy"
+            ></iframe>
+            <div className="text-[24px] md:text-[40px] bg-[#8187be67] w-full h-full flex items-center justify-end absolute px-2">
+              <span className="text-white text-[28px] md:text-[50px] font-light">
+                Kết nối với{" "}
+                <span className="font-medium text-[36px] md:text-[65px] text-[#9103FF]">
+                  SuperTech
+                </span>
+              </span>
             </div>
           </div>
-          <div className=" p-[30px] grid xl:grid-cols-2 w-full h-full absolute">
-            <div className="p-6 bg-slate-200">
-              <h1 className="text-[24px]">Biểu mẫu hỗ trợ</h1>
-
-              <form className=" grid grid-cols-3 gap-2" action="">
-                <div className=" col-span-2">
-                  <label className="text-[13px] text-[#9854CC]" htmlFor="">
-                    Email
-                  </label>
-                  <input type="text" className="w-full h-[45px]" />
-                </div>
-                <div className="">
-                  <label className="text-[13px] text-[#9854CC]" htmlFor="">
-                    Phone
-                  </label>
-                  <input type="text" className="w-full h-[45px]" />
-                </div>
-                <div>
-                  <label className="text-[13px] text-[#9854CC]" htmlFor="">
-                    Họ
-                  </label>
-                  <input type="text" className="w-full h-[45px]" />
-                </div>
-                <div className="col-span-2">
-                  <label className="text-[13px] text-[#9854CC]" htmlFor="">
-                    Tên
-                  </label>
-                  <input type="text" className="w-full h-[45px]" />
-                </div>
-                <div className="col-span-3">
-                  <label className="text-[13px] text-[#9854CC]" htmlFor="">
-                    Tiêu đề
-                  </label>
-                  <input type="text" className="w-full h-[45px]" />
-                </div>
-
-                <div className="col-span-3">
-                  <label className="text-[13px] text-[#9854CC]" htmlFor="">
-                    Nội dung
-                  </label>
-                  <textarea className="w-full h-[400px]" />
-                  <div className="bg-[#9854CC] w-full] h-[51px] flex justify-center items-center ">
-                    <span className="text-white flex justify-center items-center text-[16px] ">
-                      {" "}
-                      Gửi Mẫu
-                    </span>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div className=" flex justify-end items-end text-[26px]   ">
-              <div className="h-full flex flex-col justify-end gap-10 p-20">
-                <div className=" flex  item start b h-[full] ">
-                  <span className="text-[#7500CF]">
-                    <IoMail />
-                  </span>
-                  baochi@supertech.com
-                </div>
-                <div className="flex">
-                  <span className="text-[#00A3FF]">
-                    <FaPhone />
-                  </span>
-
-                  <span className="text-[#FC6E2E]">
-                    1900 321 123 - 180 012 334
-                  </span>
-                </div>
-                <span className="flex">
-                  <span className="text-[#FF0000]">
-                    <FaLocationCrosshairs />
-                  </span>
-                  Tan Chanh Hiep, District 12, Ho Chi Minh City, Vietnam
-                </span>
-                <span>
-                  Siêu thị SuperTech chất lượng đứng đầu về công nghệ hân hạnh
-                  được phục vụ quý khách
-                </span>
-                <div className="text-[#FF0000]">
-                  <span className="flex justify-end items-end">
-                    <span>
-                      <MdPhoneInTalk />
-                    </span>{" "}
-                    HỖ TRỢ NGAY
+        </div>
+        <div className="p-5 md:p-[30px] grid grid-cols-1 md:grid-cols-2 w-full h-full md:absolute">
+          {/* Form Section */}
+          <div className="p-4 md:p-6 bg-slate-200">
+            <h1 className="text-[20px] md:text-[24px]">Biểu mẫu hỗ trợ</h1>
+            <form className="grid grid-cols-1 md:grid-cols-3 gap-2" action="">
+              <div className="col-span-1 md:col-span-2">
+                <label className="text-[12px] md:text-[13px] text-[#9854CC]" htmlFor="">
+                  Email
+                </label>
+                <input type="text" className="w-full h-[40px] md:h-[45px]" />
+              </div>
+              <div>
+                <label className="text-[12px] md:text-[13px] text-[#9854CC]" htmlFor="">
+                  Phone
+                </label>
+                <input type="text" className="w-full h-[40px] md:h-[45px]" />
+              </div>
+              <div>
+                <label className="text-[12px] md:text-[13px] text-[#9854CC]" htmlFor="">
+                  Họ
+                </label>
+                <input type="text" className="w-full h-[40px] md:h-[45px]" />
+              </div>
+              <div className="col-span-1 md:col-span-2">
+                <label className="text-[12px] md:text-[13px] text-[#9854CC]" htmlFor="">
+                  Tên
+                </label>
+                <input type="text" className="w-full h-[40px] md:h-[45px]" />
+              </div>
+              <div className="col-span-1 md:col-span-3">
+                <label className="text-[12px] md:text-[13px] text-[#9854CC]" htmlFor="">
+                  Tiêu đề
+                </label>
+                <input type="text" className="w-full h-[40px] md:h-[45px]" />
+              </div>
+              <div className="col-span-1 md:col-span-3">
+                <label className="text-[12px] md:text-[13px] text-[#9854CC]" htmlFor="">
+                  Nội dung
+                </label>
+                <textarea className="w-full h-[200px] md:h-[400px]" />
+                <div className="bg-[#9854CC] w-full h-[45px] md:h-[51px] flex justify-center items-center mt-2">
+                  <span className="text-white text-[14px] md:text-[16px]">
+                    Gửi Mẫu
                   </span>
                 </div>
               </div>
+            </form>
+          </div>
+          {/* Contact Information Section */}
+          <div className="flex flex-col justify-end items-start text-[18px] md:text-[26px] gap-4 md:gap-6 p-4 md:p-10 text-gray-800">
+          <div className="flex items-center gap-3">
+            <span className="text-[#7500CF]">
+              <IoMail />
+            </span>
+            <span>baochi@supertech.com</span>
+          </div>
+          <div className="flex items-center gap-3">
+            <span className="text-[#00A3FF]">
+              <FaPhone />
+            </span>
+            <span className="text-[#FC6E2E]">1900 321 123 - 180 012 334</span>
+          </div>
+          <div className="flex items-center gap-3">
+            <span className="text-[#FF0000]">
+              <FaLocationCrosshairs />
+            </span>
+            <span>Tan Chanh Hiep, District 12, Ho Chi Minh City, Vietnam</span>
+          </div>
+          <span className="text-center md:text-left text-gray-600">
+            Siêu thị SuperTech chất lượng đứng đầu về công nghệ hân hạnh được phục vụ quý khách
+          </span>
+          <div className="text-[#FF0000] flex items-center gap-2 justify-center md:justify-end cursor-pointer">
+            <MdPhoneInTalk />
+            <span>HỖ TRỢ NGAY</span>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  
       <div>
-        <div className="flex flex-col justify-center items-center p-10">
-          <span className="text-[32px] font-medium p-5">
-            Chào Mừng đến với cộng đồng hỏi đáp của SuperTech
-          </span>
-          <span className="text-[16px] font-light text-[#727171] flex justify-center items-center">
-            Nơi giải đáp mọi thắc mắc của quý khách hàng
-          </span>
-        </div>
+      <div className="flex flex-col justify-center items-center p-6 md:p-10">
+  <span className="text-[24px] sm:text-[28px] md:text-[32px] font-medium p-3 md:p-5 text-center">
+    Chào Mừng đến với cộng đồng hỏi đáp của SuperTech
+  </span>
+  <span className="text-[14px] sm:text-[16px] font-light text-[#727171] text-center">
+    Nơi giải đáp mọi thắc mắc của quý khách hàng
+  </span>
+</div>
 
         <div className=" gap-3 w-full  grid xl:grid-cols-2 auto-rows-[minmax(60px,auto)]">
           <div className="grid gap-6 h-full w-full">
