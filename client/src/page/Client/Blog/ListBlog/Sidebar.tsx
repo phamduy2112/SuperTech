22996db8732a7c2aa74ddaefd62fc9d57c66fd8e
duy <duyp7454@gmail.com>
2025-03-24 08:38:51 +0700

    update code to main

--- client/src/page/Client/Blog/ListBlog/Sidebar.tsx
+++ client/src/page/Client/Blog/ListBlog/Sidebar.tsx
@@ -1,15 +1,14 @@
-import dayjs from "dayjs";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function Sidebar(props) {
   return (
-    <Link to={`/bai-viet-chi-tiet/${props.props.post_id}`}>
+    <Link to={`/bài-viết-chi-tiết/${props.props.post_id}`}>
       <div className="flex gap-4 items-start pt-2 pb-4">
         <img
           loading="lazy"
-          src={`https://res.cloudinary.com/dcvkmhlhw/image/upload/v1732821311/Blog/${props.props.media_posts?.length > 0 ? props.props.media_posts[0].media_url : ''}`}
-          alt={props.props.post_title}
+          src={`https://res.cloudinary.com/dcvkmhlhw/image/upload/v1732821311/Blog/${props.props.media_url}`}
+          alt={""}
           className="object-cover w-20 h-20 rounded"
         />
         <div className="flex flex-col">
@@ -19,7 +18,7 @@ function Sidebar(props) {
               : props.props.post_title}
           </h3>
           <div className="text-[12px] text-neutral-500 line-clamp-2">
-            <span>📅 {dayjs(props.props.post_date).format('YYYY-MM-DD HH:mm:ss')}</span>
+            <span>📅 {props.props.post_date}</span> ·{" "}
             {/* <span>✆ {props.props.post_content}</span> */}
           </div>
         </div>
